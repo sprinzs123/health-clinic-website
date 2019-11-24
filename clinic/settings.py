44cc40f20a05cd8664475e09ad647723eecbc996
Your Name <you@example.com>
2019-11-23 18:59:09 -0600

    fixed static files

--- clinic/settings.py
+++ clinic/settings.py
@@ -122,7 +122,7 @@ os.path.join(BASE_DIR, 'clinic/static/')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
